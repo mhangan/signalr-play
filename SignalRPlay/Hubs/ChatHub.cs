b9957dc8cc0cb688e0d2112b8217139302a3ea68
Mihai Viorel Hangan <mihai.hangan@gmail.com>
2018-03-01 23:44:32 +0200

    Implement sending private messages.

--- SignalRPlay/Hubs/ChatHub.cs
+++ SignalRPlay/Hubs/ChatHub.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
 namespace SignalRPlay
 {
 	public class ChatHub : Hub
 	{
+		private static readonly Dictionary<string, dynamic> _registeredUsers = new Dictionary<string, dynamic>();
 		public void Send(string name, string message)
 		{
 			// Call the addNewMessageToPage method to update clients.
-			Clients.All.addNewMessageToPage(name, message);
+			Clients.All.addNewMessageToPage(name, message, false);
+		}
+
+		public void Register(string username)
+		{
+			_registeredUsers[username] = Clients.Caller;
+		}
+
+		public void SendPrivate(string from, string to, string message)
+		{
+			if (_registeredUsers.ContainsKey(to))
+			{
+				dynamic toUser = _registeredUsers[to];
+				toUser.addNewMessageToPage(from, message, true);
+			}
 		}
 	}
 }
