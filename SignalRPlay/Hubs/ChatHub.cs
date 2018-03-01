cb4e6de39d30f6860001a7a154fe09349445f5ee
Mihai Viorel Hangan <mihai.hangan@gmail.com>
2018-03-01 22:46:04 +0200

    Add bootstrap, jquery, layout, home index and chat views.

---
+++ SignalRPlay/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+namespace SignalRPlay
+{
+	public class ChatHub : Hub
+	{
+		public void Send(string name, string message)
+		{
+			// Call the addNewMessageToPage method to update clients.
+			Clients.All.addNewMessageToPage(name, message);
+		}
+	}
+}
