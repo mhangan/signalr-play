933f3188e50ca4853a6c1b010488530e45babc24
m.hangan <m.hangan@netmatch.ro>
2018-03-02 17:04:52 +0200

    Use Redis for scaling out and groups for private messages.

--- SignalRPlay/Hubs/ChatHub.cs
+++ SignalRPlay/Hubs/ChatHub.cs
@@ -1,28 +1,38 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNet.SignalR;
+﻿using Microsoft.AspNet.SignalR;
+
 namespace SignalRPlay
 {
 	public class ChatHub : Hub
 	{
-		private static readonly Dictionary<string, dynamic> _registeredUsers = new Dictionary<string, dynamic>();
+		/// <summary>
+		/// Broadcast the message to all users.
+		/// </summary>
+		/// <param name="name">Name of user who sent the message</param>
+		/// <param name="message">The message</param>
 		public void Send(string name, string message)
 		{
 			// Call the addNewMessageToPage method to update clients.
 			Clients.All.addNewMessageToPage(name, message, false);
 		}
 
+		/// <summary>
+		/// Register a group with the user name, to enable receiving private messages.
+		/// </summary>
+		/// <param name="username">The username</param>
 		public void Register(string username)
 		{
-			_registeredUsers[username] = Clients.Caller;
+			Groups.Add(Context.ConnectionId, username);
 		}
 
+		/// <summary>
+		/// Sends a private message to the user 
+		/// </summary>
+		/// <param name="from">Sender of the message</param>
+		/// <param name="to">Receiver of the message</param>
+		/// <param name="message">The message</param>
 		public void SendPrivate(string from, string to, string message)
 		{
-			if (_registeredUsers.ContainsKey(to))
-			{
-				dynamic toUser = _registeredUsers[to];
-				toUser.addNewMessageToPage(from, message, true);
-			}
+			Clients.Group(to).addNewMessageToPage(from, message, true);
 		}
 	}
 }
--- SignalRPlay/Startup.cs
+++ SignalRPlay/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Owin;
 using Microsoft.Owin;
+
 [assembly: OwinStartup(typeof(SignalRPlay.Startup))]
 namespace SignalRPlay
 {
@@ -8,6 +10,8 @@ namespace SignalRPlay
 		public void Configuration(IAppBuilder app)
 		{
 			// Any connection or hub wire up and configuration should go here
+			// Use Redis for scaling out to multiple servers.
+			GlobalHost.DependencyResolver.UseRedis("NMT-RED-202", 6379, "", "SignalRPlay");
 			app.MapSignalR();
 		}
 	}
